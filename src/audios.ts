63d0d173228b438edd10eed382141ee76a79d06f
pacexy <pacexy@gmail.com>
2022-12-25 11:47:49 +0800

    feat: play audio

---
+++ src/audios.ts
@@ -0,0 +1,2 @@
+// https://free-loops.com/3396-canned-laugh.html
+export { default as cannedLaughter } from '../public/audios/canned-laughter.mp3'
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,22 @@
-// @ts-ignore isolatedModules
-console.log('hello world');
+import { cannedLaughter } from './audios'
+
+function createAudio() {
+  const audio = document.createElement('audio')
+
+  audio.id = 'canned-laughter'
+  audio.src = cannedLaughter
+  audio.volume = 0.5
+  document.body.append(audio)
+
+  return audio
+}
+
+const audio = createAudio()
+
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'h') {
+    audio.play()
+  }
+})
+
+console.log('[canned-laughter] loaded')
