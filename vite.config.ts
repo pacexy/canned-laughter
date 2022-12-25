3faa68b321085717b70f8b9e8b83ff11ef74fe10
pacexy <pacexy@gmail.com>
2022-12-25 13:33:53 +0800

    chore: update description

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,10 @@ export default defineConfig({
         icon: 'https://raw.githubusercontent.com/pacexy/canned-laughter/main/public/images/1f604.png',
         namespace: 'npm/vite-plugin-monkey',
         match: ['*://www.bilibili.com/bangumi/play/ep*'],
+        description: {
+          'en-US': 'Add canned laughter to web',
+          'zh-CN': '为网页添加罐头笑声',
+        },
       },
     }),
   ],
