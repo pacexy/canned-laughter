b1b23104467b5abb0c533266d80998df6ebdbd2e
pacexy <pacexy@gmail.com>
2022-12-25 12:58:17 +0800

    chore: update match

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import monkey from 'vite-plugin-monkey';
+import { defineConfig } from 'vite'
+import monkey from 'vite-plugin-monkey'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,8 +9,8 @@ export default defineConfig({
       userscript: {
         icon: 'https://vitejs.dev/logo.svg',
         namespace: 'npm/vite-plugin-monkey',
-        match: ['https://www.google.com/'],
+        match: ['*://www.bilibili.com/bangumi/play/ep*'],
       },
     }),
   ],
-});
+})
